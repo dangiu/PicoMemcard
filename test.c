6836df1eeccb3a1a965c7316bff16bdcfa8732cc
Beaglehot <danielegiuliani0@gmail.com>
2022-04-08 13:50:49 +0200

    Update IDE configuration, add PIO sm supporting modified psx spi, add showcase of joypad toggle

--- test.c
+++ test.c
@@ -8,122 +8,179 @@
 #define PIN_CMD 6
 #define PIN_SEL 7
 #define PIN_CLK 8
-
 #define PIN_ACK 9
+
+#define PIN_LED 25
 
 #define ID_LO 0x41
 #define ID_HI 0x5A
 
-#define R_PRESSED 0b11111011
-#define L_PRESSED 0b11111110
+#define R_PRESSED 0b11011111
+#define L_PRESSED 0b01111111
 #define NO_PRESSED 0xff
 #define SEND_ACK 0b100000000
 
 
-uint smReadCmd;
-uint smSendAck;
-uint smWriteDat;
+PIO pio = pio0;
 
+uint smSelMonitor;
+uint smCmdReader;
+uint smAckSender;
+uint smDatWriter;
 
+uint offsetSelMonitor;
+uint offsetCmdReader;
+uint offsetDatWriter;
+uint offsetAckSender;
+
+static uint count = 0;
+
+/**
+ * @brief Interrupt handler called when SEL goes high
+ * Resets ackSender, cmdReader and datWriter state machines
+ */
 void pio0_irq0() {
-	/* SEL has gone high, reset ISR of smReadCmd (could contain garbage) before it goes low again*/
-	pio_sm_restart(pio0, smReadCmd);
-	irq_clear(PIO0_IRQ_0);	// clear IRQ flag
+	pio_set_sm_mask_enabled(pio, 1 << smCmdReader | 1 << smDatWriter, false);
+	pio_restart_sm_mask(pio, 1 << smCmdReader | 1 << smDatWriter);
+    pio_sm_exec(pio, smCmdReader, pio_encode_jmp(offsetCmdReader));	// restart smCmdReader PC
+    pio_sm_exec(pio, smDatWriter, pio_encode_jmp(offsetDatWriter));	// restart smDatWriter PC
+    //pio_sm_exec(pio, smAckSender, pio_encode_jmp(offsetAckSender));	// restart smAckSender PC
+	pio_enable_sm_mask_in_sync(pio, 1 << smCmdReader | 1 << smDatWriter);
 	pio_interrupt_clear(pio0, 0);
 }
 
-void process_joy_req(PIO pio) {
-	sendAck(pio);	// acknowledge top command
+void cancel_ack() {
+	//pio_sm_exec(pio, smAckSender, pio_encode_jmp(offsetCmdReader));		// restart smCmdReader
+}
 
-	uint32_t next_byte = 0;
-	sendByte(pio, smWriteDat, ID_LO);	// write lower ID byte
+void process_joy_req(uint8_t next_byte) {
+	printf("\n%.2x ", next_byte);
+	write_dat_LSB_blocking(pio, smDatWriter, ID_LO);	// write lower ID byte
 
-	next_byte = pio_sm_get_blocking(pio, smReadCmd) >> 24;	// shift to get MSB
+	next_byte = read_cmd_byte_blocking(pio, smCmdReader);
 	if(next_byte != 0x42) {
 		printf("\nWaiting for %.2x but received %.2x\n", 0x42, next_byte);
 		return;
 	} else {
+		write_dat_LSB_blocking(pio, smDatWriter, ID_HI);	// write upper ID byte
 		printf("%.2x ", next_byte);
 	}
-	sendAck(pio);
-	sendByte(pio, smWriteDat, ID_HI);	// write upper ID byte
 
-	next_byte = pio_sm_get_blocking(pio, smReadCmd) >> 24;	// shift to get MSB
+	next_byte = read_cmd_byte_blocking(pio, smCmdReader);
+	if(next_byte != 0x00) {
+		printf("\nWaiting for %.2x but received %.2x\n", 0x00, next_byte);
+		return;
+	} else {
+		switch (count) {
+		case 0:
+			write_dat_LSB_blocking(pio, smDatWriter, R_PRESSED);	// fake right d-pad being pressed
+			break;
+		case 2:
+			write_dat_LSB_blocking(pio, smDatWriter, L_PRESSED);	// fake left d-pad being pressed
+			break;
+		case 1:
+		case 3:		
+		default:
+			write_dat_LSB_blocking(pio, smDatWriter, NO_PRESSED);	// release all buttons
+			break;
+		}
+		printf("%.2x ", next_byte);
+	}
+
+	next_byte = read_cmd_byte_blocking(pio, smCmdReader);
+	if(next_byte != 0x00) {
+		printf("\nWaiting for %.2x but received %.2x\n", 0x00, next_byte);
+		return;
+	} else {
+		write_dat_LSB_blocking(pio, smDatWriter, NO_PRESSED);	// other buttons are all inactive
+		printf("%.2x ", next_byte);
+	}
+
+	next_byte = read_cmd_byte_blocking(pio, smCmdReader);
+	cancel_ack();	// last byte being received, no need to send more data
 	if(next_byte != 0x00) {
 		printf("\nWaiting for %.2x but received %.2x\n", 0x00, next_byte);
 		return;
 	} else {
 		printf("%.2x ", next_byte);
 	}
-	sendAck(pio);
-	sendByte(pio, smWriteDat, R_PRESSED);	// fake right d-pad being pressed
-
-	next_byte = pio_sm_get_blocking(pio, smReadCmd) >> 24;	// shift to get MSB
-	if(next_byte != 0x00) {
-		printf("\nWaiting for %.2x but received %.2x\n", 0x00, next_byte);
-		return;
-	} else {
-		printf("%.2x ", next_byte);
-	}
-	sendAck(pio);
-	sendByte(pio, smWriteDat, NO_PRESSED);	// other buttons are all inactive
-
-	next_byte = pio_sm_get_blocking(pio, smReadCmd) >> 24;	// shift to get MSB
-	if(next_byte != 0x00) {
-		printf("\nWaiting for %.2x but received %.2x\n", 0x00, next_byte);
-		return;
-	} else {
-		printf("%.2x ", next_byte);
-	}
-	// no ack and no send, transmission is over		
 }
 
 int main() {
 	stdio_init_all();
-	PIO pio = pio0;
 
-	uint offsetReadCmd = pio_add_program(pio, &readCmd_program);
-	uint offsetSendAck = pio_add_program(pio, &sendAck_program);
-	uint offsetWriteDat = pio_add_program(pio, &writeDat_program);
+	printf("\n\nBeginning Execution...\n");
 
-	smReadCmd = pio_claim_unused_sm(pio, true);
-	smSendAck = pio_claim_unused_sm(pio, true);
-	smWriteDat = pio_claim_unused_sm(pio, true);
-
-	sendAck_program_init(pio, smSendAck, offsetSendAck, PIN_ACK);
-	writeDat_program_init(pio, smWriteDat, offsetWriteDat, PIN_DAT, PIN_SEL);
-	readCmd_program_init(pio, smReadCmd, offsetReadCmd, PIN_CMD);
-
-	/*
-	selMonitor_program_init(pio, smSelMonitor, offsetSelMonitor, PIN_SEL);
-
+	/* Setup PIO interrupts */
 	irq_set_exclusive_handler(PIO0_IRQ_0, pio0_irq0);
-	pio_set_irq0_source_enabled(pio, pis_interrupt0, true);
 	irq_set_enabled(PIO0_IRQ_0, true);
-	*/
 
+	offsetSelMonitor = pio_add_program(pio, &sel_monitor_program);
+	offsetCmdReader = pio_add_program(pio, &cmd_reader_program);
+	offsetAckSender = pio_add_program(pio, &ack_sender_program);
+	offsetDatWriter = pio_add_program(pio, &dat_writer_program);
+
+	smSelMonitor = pio_claim_unused_sm(pio, true);
+	smCmdReader = pio_claim_unused_sm(pio, true);
+	smAckSender = pio_claim_unused_sm(pio, true);
+	smDatWriter = pio_claim_unused_sm(pio, true);
+
+	dat_writer_program_init(pio, smDatWriter, offsetDatWriter, PIN_DAT, PIN_CLK);
+	ack_sender_program_init(pio, smAckSender, offsetAckSender, PIN_ACK);
+	cmd_reader_program_init(pio, smCmdReader, offsetCmdReader, PIN_CMD);
+	sel_monitor_program_init(pio, smSelMonitor, offsetSelMonitor, PIN_SEL);
+
+	
+	// test attempting to write TX before program is even started
+	// allows us to observe is some iterations are capable of sending stuff
 	/*
+	write_dat_LSB_blocking(pio, smDatWriter, 0xff);
+	write_dat_LSB_blocking(pio, smDatWriter, ID_LO);
+	write_dat_LSB_blocking(pio, smDatWriter, ID_HI);
+	write_dat_LSB_blocking(pio, smDatWriter, 0xff);
+	write_dat_LSB_blocking(pio, smDatWriter, 0xff);
+	*/
+	//write_dat_LSB_blocking(pio, smDatWriter, 0xff);
+
+
+	/* Enable all SM simultaneously */
+	uint32_t smMask = (1 << smSelMonitor) | (1 << smCmdReader) | (1 << smAckSender) | (1 << smDatWriter);
+	pio_enable_sm_mask_in_sync(pio, smMask);
+
 	uint32_t i = 0;
 	while(true) {
-		if(i % 1000000 == 0) {
-			sendAck(pio);
+		uint8_t item = read_cmd_byte_blocking(pio, smCmdReader);
+		//send_ack(pio);
+		/*
+		if(i == 0) {
+			write_dat_LSB_blocking(pio, smDatWriter, ID_LO);
+			write_dat_LSB_blocking(pio, smDatWriter, ID_HI);
+			i++;
 		}
-		i++;
-	}
-	*/
+		*/
 
-	//printf("\nSetup done, starting loop...\n");
-
-	pio_sm_set_enabled(pio, smReadCmd, true);
-	while(true) {
-		uint32_t item = pio_sm_get_blocking(pio, smReadCmd);
-		item >>= 24;	// shift to get MSB
-
-		/* PRINT CMD RECEIVED */
-		printf("\n%.2x ", item);
+		/*
+		switch (item) {
+		case 0x01:
+			write_dat_LSB_blocking(pio, smDatWriter, ID_LO);
+			break;
+		case 0x42:
+			write_dat_LSB_blocking(pio, smDatWriter, ID_HI);
+			break;
+		case 0x00:
+			write_dat_LSB_blocking(pio, smDatWriter, 0xff);
+			break;
+		default:
+			break;
+		}
+		*/
+		
 
 		if(item == 0x01)
-			process_joy_req(pio);
+			process_joy_req(item);
+			count = (count + 1) % 4;
 
+			//printf("\n");
+		//printf("%.2x ", item);
 	}
 }
