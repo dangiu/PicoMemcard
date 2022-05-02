e0fcfd76e2c009adc2cb9dae9e6bbe5e370ec60b
Beaglehot <danielegiuliani0@gmail.com>
2022-05-02 15:39:04 +0200

    Update memcard sniffer utility, add freepsxboot raw image

--- memcard_sniffer.c
+++ memcard_sniffer.c
@@ -1,7 +1,7 @@
 /**
- * @file memcard_simulator.c
+ * @file protocol_sniffer.c
  * @author Daniele Giuliani (danielegiuliani0@gmail.com)
- * @brief Simulating a PSX Memory Card using a PIO
+ * @brief Utility used to sniff PSX SPI protocol (CMD and DAT lines)
  * @version 0.1
  * @date 2022-04-08
  */
@@ -11,19 +11,12 @@
 #include "hardware/pio.h"
 #include "hardware/irq.h"
 #include "psxSPI.pio.h"
-#include "memory_card.h"
-#include "mc_raw.h"
 
 #define PIN_DAT 5
 #define PIN_CMD 6
 #define PIN_SEL 7
 #define PIN_CLK 8
 #define PIN_ACK 9
-
-#define MEMCARD_TOP 0x81
-#define MEMCARD_READ 0x52
-#define MEMCARD_WRITE 0x57
-#define MEMCARD_ID 0x53
 
 #define BUFF_LEN 4096
 
@@ -52,23 +45,9 @@ void pio0_irq0() {
 	pio_restart_sm_mask(pio, 1 << smCmdReader | 1 << smDatReader);
 	pio_sm_exec(pio, smCmdReader, pio_encode_jmp(offsetCmdReader));	// restart smCmdReader PC
 	pio_sm_exec(pio, smDatReader, pio_encode_jmp(offsetDatReader));	// restart smDatReader PC
-
 	pio_interrupt_clear(pio0, 0);
 	pio_enable_sm_mask_in_sync(pio, 1 << smCmdReader | 1 << smDatReader);
 	restartProto = true;
-}
-
-void suspend_proto() {
-	pio_set_sm_mask_enabled(pio, 1 << smCmdReader | 1 << smDatReader, false);
-	pio_restart_sm_mask(pio, 1 << smCmdReader | 1 << smDatReader);
-}
-
-void restart_proto() {
-	pio_sm_clear_fifos(pio, smCmdReader);
-	pio_sm_clear_fifos(pio, smDatReader);
-	pio_sm_exec(pio, smCmdReader, pio_encode_jmp(offsetCmdReader));	// restart smCmdReader PC
-	pio_sm_exec(pio, smDatReader, pio_encode_jmp(offsetDatReader));	// restart smDatReader PC
-	pio_enable_sm_mask_in_sync(pio, 1 << smCmdReader | 1 << smDatReader);
 }
 
 int main() {
@@ -92,23 +71,18 @@ int main() {
 	cmd_reader_program_init(pio, smCmdReader, offsetCmdReader, PIN_CMD);
 	sel_monitor_program_init(pio, smSelMonitor, offsetSelMonitor, PIN_SEL);
 
-
 	/* Enable all SM simultaneously */
 	uint32_t smMask = (1 << smSelMonitor) | (1 << smCmdReader) | (1 << smDatReader);
 	pio_enable_sm_mask_in_sync(pio, smMask);
 
-	int index = 0;
-
-	while(true) {
-		cmdBuffer[index] = read_cmd_byte_blocking(pio, smCmdReader);
-		datBuffer[index] = read_cmd_byte_blocking(pio, smDatReader);
-		index++;
-		if(index >= BUFF_LEN) {
-			break;
-		}
+	/* Samping phase */
+	for(int i = 0; i < BUFF_LEN; ++i) {
+		cmdBuffer[index] = read_byte_blocking(pio, smCmdReader);
+		datBuffer[index] = read_byte_blocking(pio, smDatReader);
 	}
 
-	for(int i = 0; i < BUFF_LEN; i++) {
+	/* Printing results */
+	for(int i = 0; i < BUFF_LEN; ++i) {
 		printf("\t%.2x\t%.2x\n", cmdBuffer[i], datBuffer[i]);
 	}
 }
