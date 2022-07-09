b097a36b7285d7e8e508b75a33c4a8816dbd2e33
Darren Anderson <darrena092@gmail.com>
2022-07-09 21:02:19 +0100

    Fix page sync size.

--- src/memory_card.c
+++ src/memory_card.c
@@ -56,7 +56,7 @@ uint32_t memory_card_sync_page(MemoryCard *mc, uint16_t address, uint8_t* data) 
 	if(LFS_ERR_OK == lfs_mount(&lfs, &LFS_CFG)) {
 		if(LFS_ERR_OK == lfs_file_open(&lfs, &memcard, MEMCARD_FILE_NAME, LFS_O_RDWR)) {
 			lfs_file_seek(&lfs, &memcard, address, LFS_SEEK_SET);
-			if(MC_SIZE != lfs_file_write(&lfs, &memcard, data, MC_SEC_SIZE)) {
+			if(MC_SEC_SIZE != lfs_file_write(&lfs, &memcard, data, MC_SEC_SIZE)) {
 				status = 1;	// failed to write-back memory card image
 			}
 			lfs_file_close(&lfs, &memcard);
