3e458b46f2b9526793d2939757573a6162102934
Beaglehot <danielegiuliani0@gmail.com>
2022-08-26 16:04:25 +0200

    Add controller input (STRT+SEL+TRNGL) to create new empty memory card image and switch to it, closes #21

--- inc/memcard_manager.h
+++ inc/memcard_manager.h
@@ -9,6 +9,10 @@
 #define MM_ALLOC_FAIL			1
 #define MM_INDEX_OUT_OF_BOUNDS	2
 #define MM_NO_ENTRY				3
+#define MM_BAD_PARAM			4
+#define MM_NAME_CONFLICT		5
+#define MM_FILE_OPEN_ERR		6
+#define MM_FILE_WRITE_ERR		7
 
 bool memcard_manager_exist(uint8_t* filename);
 uint32_t memcard_manager_count();
@@ -16,6 +20,6 @@ uint32_t memcard_manager_get(uint32_t index, uint8_t* out_filename);
 #define memcard_manager_get_first(out_filename) memcard_manager_get(0, (out_filename))
 uint32_t memcard_manager_get_next(uint8_t* filename, uint8_t* out_nextfile);
 uint32_t memcard_manager_get_prev(uint8_t* filename, uint8_t* out_prevfile);
-uint32_t memcard_manager_create();
+uint32_t memcard_manager_create(uint8_t* out_filename);
 
 #endif
